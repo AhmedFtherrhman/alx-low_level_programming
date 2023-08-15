6b53f06e304a31a828862786c631602a4ec225d0
AhmedFtherrhman <ahmedftherrhman97@gmail.com>
2023-08-15 06:56:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,2 +1,12 @@
-print_alphabet();
-return (0);
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
